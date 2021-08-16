f77a39a40745a884b63db688a1ecfb7da87488f7
tpitkanen <55845778+tpitkanen@users.noreply.github.com>
2021-08-16 21:04:00 +0300

    Update code format

--- Rename-FFXIVScreenshots.ps1
+++ Rename-FFXIVScreenshots.ps1
@@ -1,12 +1,12 @@
 function Rename-FFXIVScreenshots {
-    # Rename FFXIV screenshots from "ffxiv_{dMy_hms_S}.{ext}" to "ff_{yMd_hms_S}.{ext}"
+    # Rename FFXIV screenshots from 'ffxiv_ddMMyyyy_hhmmss_SSS.ext' to 'ff_yyyyMMdd_hhmmss_SSS.ext'
     # e.g. 'ffxiv_31072021_164028_968.png' -> 'ff_20210731_164028_968.png'
     $screenshot_folder = '.'
     $items = Get-ChildItem -Path $screenshot_folder -Recurse -File -Include 'ffxiv_*.png', 'ffxiv_*.jpg'
 
     foreach ($i in $items) {
         $old_name = $i.Name
-        $new_name = $i.Name -Replace 'ffxiv_(\d{2})(\d{2})(\d{4})_(\d{2})(\d{2})(\d{2})', 'ff_$3$2$1_$4$5$6'
+        $new_name = $i.Name -replace 'ffxiv_(\d{2})(\d{2})(\d{4})_(\d{2})(\d{2})(\d{2})', 'ff_$3$2$1_$4$5$6'
 
         if ($new_name -ne $old_name) {
             Rename-Item -Path $i -NewName $new_name
--- Rename-FFXIVScreenshotsBack.ps1
+++ Rename-FFXIVScreenshotsBack.ps1
@@ -1,12 +1,12 @@
 function Rename-FFXIVScreenshotsBack {
-    # Rename FFXIV screenshots from "ff_{yMd_hms_S}.{ext}" to "ffxiv_{dMy_hms_S}.{ext}"
+    # Rename FFXIV screenshots from 'ff_yyyyMMdd_hhmmss_SSS.ext' to 'ffxiv_ddMMyyyy_hhmmss_SSS.ext'
     # e.g. 'ff_20210731_164028_968.png' -> 'ffxiv_31072021_164028_968.png'
     $screenshot_folder = '.'
     $items = Get-ChildItem -Path $screenshot_folder -Recurse -File -Include 'ff_*.png', 'ff_*.jpg'
 
     foreach ($i in $items) {
         $old_name = $i.Name
-        $new_name = $i.Name -Replace 'ff_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})', 'ffxiv_$3$2$1_$4$5$6'
+        $new_name = $i.Name -replace 'ff_(\d{4})(\d{2})(\d{2})_(\d{2})(\d{2})(\d{2})', 'ffxiv_$3$2$1_$4$5$6'
 
         if ($new_name -ne $old_name) {
             Rename-Item -Path $i -NewName $new_name
